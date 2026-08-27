4e54c4c2079bad1d4b410eb767ba60ffe39863cb
si-eyy <24-09048@g.batstate-u.edu.ph>
2026-08-27 16:47:29 +0800

    fix: correct typo in output message of MyNewRepo.java

--- August 27, 2026/MyNewRepo.java
+++ August 27, 2026/MyNewRepo.java
@@ -1,5 +1,5 @@
 class MyNewRepo {
     public static void main (String[]args) {
-        System.out.println("I will be adding this to file to my new repo-");
+        System.out.println("I will be adding this file to my new repo-");
     }
 }
